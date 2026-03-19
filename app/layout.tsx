1d9b03234ee07a5a52959bbcac376b4742e17b66
Seira <seira@seira-laptop.local>
2026-03-19 11:25:19 +0700

    Add sun favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
       </head>
       <body>
